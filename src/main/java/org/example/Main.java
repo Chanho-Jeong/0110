3caf5cbcd5f403aff8449d7fa226466554322912
zerock <kangckck24@gmail.com>
2025-01-10 15:37:30 +0900

    21

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.example.save.SaveService;
+import org.example.save.SaveUI;
 
 import java.util.Scanner;
 
@@ -11,37 +12,10 @@ public class Main {
 
         //필요한 객체 준비
         //프로그램 시작
-        SaveService save = new SaveService();
-        Scanner scanner = new Scanner(System.in);
-
-        //입금 1, 출금 2
-        System.out.println("1.입금, 2.출금, -1:종료");
-
-        String oper = scanner.nextLine();
-
-        switch (oper){
-            case "1":
-                System.out.println("얼마를 입금?");
-                int value = Integer.parseInt(scanner.nextLine());
-
-                int current = save.in(value);
-                System.out.println("현재 잔고는: " + current);
-
-                break;
-            case "2":
-                break;
-            case "-1":
-                break;
-        }
+        SaveUI ui = new SaveUI();
+        ui.doJob();
 
 
-        //만일 입금이라면
-        //얼마를 추가할까요?
-        // save.in( )호출
-
-        //만일 출금이라면
-        //진짜 꺼낼껀가요?
-        //save.out( )호출
 
 
     }
