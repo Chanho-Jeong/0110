0020ce7f18f586ef0629b7c8bc7d94738de65a7f
zerock <kangckck24@gmail.com>
2025-01-10 13:52:39 +0900

    Initial commit

---
+++ src/main/java/org/example/word/WordService.java
@@ -0,0 +1,18 @@
+package org.example.word;
+
+public class WordService {
+
+
+
+    public WordService(){
+
+    }
+
+    //다음 단어를 뽑아주는 기능
+    public WordVO getNextWord() {
+        WordVO word = null;
+
+        return word;
+    }
+
+}
---
+++ src/main/java/org/example/word/WordVO.java
@@ -0,0 +1,4 @@
+package org.example.word;
+
+public class WordVO {
+}
